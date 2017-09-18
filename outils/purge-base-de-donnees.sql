3d82f96f46ba7192a2f5b3d28161439e66d8bded
Tony Galmiche <tony.galmiche@infosaone.com>
2017-09-18 15:00:33 +0200

    add outils

---
+++ outils/purge-base-de-donnees.sql
@@ -0,0 +1,30 @@
+
+-- Stocks --
+delete from stock_move;
+delete from stock_quant;
+delete from stock_production_lot;
+delete from stock_inventory;
+
+
+-- Factures --
+delete from account_invoice;
+
+
+-- Livraisons --
+delete from stock_pack_operation;
+delete from stock_picking;
+
+
+-- Compta --
+delete from account_move;
+delete from account_move_line;
+
+
+-- Commandes --
+delete from sale_order;
+
+
+-- Commandes Fournisseur --
+delete from purchase_order;
+
+
